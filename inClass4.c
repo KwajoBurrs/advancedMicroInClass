f30d9d03777700134ebbec8f980f3b6c3752e83f
KwajoBurrs <86699656+KwajoBurrs@users.noreply.github.com>
2025-03-30 16:15:06 -0400

    Update inClass4.c

--- inClass4.c
+++ inClass4.c
@@ -31,22 +31,28 @@ int main(void)
     WDTCTL = 0x5A80;
 
     //Configure PMW Output for TA0 CCR1 on P2.4
-    P2DIR  |= 0b00010000;
-    P2SEL0 |= 0b00010000;
+    //P2DIR  |= 0b00010000;
+    P2DIR |= 0x10;
+    //P2SEL0 |= 0b00010000;
+    P2SEL0 |= 0x10;
 
     //Configure
     P7DIR  &= ~BIT7;
-    P7SEL0 |= 0b10000000;
+    //P7SEL0 |= 0b10000000;
+    P7SEL0 |= 0x80;
 
     //Configure Timer A0
-    TA0CTL = 0b0000001000010110; // (up mode, SMCLK, divider: 1)
+    //TA0CTL = 0b0000001000010110; // (up mode, SMCLK, divider: 1)
+    TA0CTL = 0x0216;
     TA0CCR0 = 64000; //PERIOD
     TA0CCR1 = 30000; //DUTY
     TA0CCTL1 = 0b0000100011000000; // (compare mode, toggle/set)
 
     //Configure Timer A1
-    TA1CTL = 0b0000001000100100; // (continuous, SMCLK, divider: 1)
-    TA1CCTL1 = 0b0100100100010000; // (capture mode)
+    //TA1CTL = 0b0000001000100100; // (continuous, SMCLK, divider: 1)
+    TA1CTL = 0x0224;
+    //TA1CCTL1 = 0b0100100100010000; // (capture mode)
+    TA1CCTL1 = 0x4910;
 
     while(x != NUMBEROFLOOPS){
 
@@ -54,15 +60,18 @@ int main(void)
         if((TA1CCTL1 & BIT0) == 1)
         {
             x++;
-            TA1CTL = 0b0000001000000100; // Stop TA1
+            //TA1CTL = 0b0000001000000100; // Stop TA1
+            TA1CTL = 0x0204;
             TA1CCTL1 = 0b00011100100010000; // Disconnect Capture, Input VCC, Clear Int
             TA0CTL &= ~BIT0;// Clear TA0 Int
             period = TA1CCR1;
 
             while((TA0CTL & BIT0) != 1); // Wait for next TA0 to start again
 
-            TA1CTL = 0b0000001000100100; // Start TA1 with initial config
-            TA1CCTL1 = 0b0100100100010000;
+            //TA1CTL = 0b0000001000100100; // Start TA1 with initial config
+            TA1CTL = 0x0224;
+            //TA1CCTL1 = 0b0100100100010000;
+            TA1CCTL1 = 0x4910;
 
 
         }
@@ -71,13 +80,18 @@ int main(void)
 
     puts("EEL4742/5769 -- Advanced Microprocessors\n");
     puts("Spring 2025");
-    puts("Hezekiah Destinoble");
+    puts("Kwajo Burrs");
     puts("Date: 03/28/25");
     puts("TA0 Divider is: 1");
     puts("TA1 Divider is: 1");
     printf("The Period is %d\n", period);
 
-    while(1);
+    while(1)
+    {
+    
+    }
+        
+
 
 } // END OF MAIN
 
